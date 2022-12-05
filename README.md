bbad7b8d70e30a1972d6f0a432f0474f526f7e3b
aftab khan <aftabk@zeta.tech>
2022-12-05 16:08:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSNetwork
+
+A description of this package.
---
+++ Tests/CSNetworkTests/CSNetworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CSNetwork
+
+final class CSNetworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CSNetwork().text, "Hello, World!")
+    }
+}
