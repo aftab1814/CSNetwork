1a2546abb426004addab065995886046ad8e5219
aftab khan <aftabk@zeta.tech>
2022-12-08 16:53:14 +0530

    Update package

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CSNetwork",
-            dependencies: []),
+            name: "CSNetwork"),
         .testTarget(
             name: "CSNetworkTests",
             dependencies: ["CSNetwork"]),
+        .binaryTarget(name: "CSNetwork", path: "artifacts/CSNetwork.xcframework")
     ]
 )
