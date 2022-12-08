705cd53f6fb273df53f3b4b67abbd0e4adf12d77
aftab khan <aftabk@zeta.tech>
2022-12-08 20:29:05 +0530

    Add newtwork kit

--- Sources/CSNetworkKit.xcframework/ios-arm64_x86_64-simulator/CSNetworkKit.framework/Core/Extensions/Bundle+Extension.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension Bundle {
-    var displayName: String? {
-        return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
-    }
-}
--- Sources/CSNetworkKit.xcframework/ios-arm64_x86_64-simulator/CSNetworkKit.framework/Core/Network/Error/ServiceError.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum ServiceError: Error {
-    case unknown
-    case failed(Int, Data?)
-    case noJSONData
-    case invalidJSONData
-    case typeMismatch(String)
-}
