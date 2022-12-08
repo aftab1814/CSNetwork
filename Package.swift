ae3449e249b338fe2817c7f34a02e419c0b20169
aftab khan <aftabk@zeta.tech>
2022-12-08 18:51:16 +0530

    Add xcframework

--- Package.swift
+++ Package.swift
@@ -16,10 +16,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CSNetwork"),
         .testTarget(
             name: "CSNetworkTests",
-            dependencies: ["CSNetwork"])
+            dependencies: ["CSNetwork"]),
+        .binaryTarget(name: "CSNetwork", path: "Sources/CSNetwork.xcframework")
     ]
 )
---
+++ Sources/CSNetwork.xcframework/ios-arm64/CSNetwork.framework/Headers/CSNetwork.h
@@ -0,0 +1,18 @@
+//
+//  CSNetwork.h
+//  CSNetwork
+//
+//  Created by Aniket Bidwai on 08/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CSNetwork.
+FOUNDATION_EXPORT double CSNetworkVersionNumber;
+
+//! Project version string for CSNetwork.
+FOUNDATION_EXPORT const unsigned char CSNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSNetwork/PublicHeader.h>
+
+
